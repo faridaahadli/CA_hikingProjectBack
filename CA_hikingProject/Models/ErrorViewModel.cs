83142d8fc4b9ac911e5f63734c149e78d5ae9978
Farida Ahadli <faridaahadli59@gmail.com>
2020-02-03 15:35:51 +0400

    Add project files.

---
+++ CA_hikingProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CA_hikingProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
